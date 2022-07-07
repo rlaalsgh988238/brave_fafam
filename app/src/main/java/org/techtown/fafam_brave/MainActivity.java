03449e0e6d99ad351249312b8a751533afac34b0
rlaalsgh988238 <58495097+rlaalsgh988238@users.noreply.github.com>
2022-07-07 22:42:33 +0900

    down!

--- app/src/main/java/org/techtown/fafam_brave/MainActivity.java
+++ app/src/main/java/org/techtown/fafam_brave/MainActivity.java
@@ -10,6 +10,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //sdf_well?????
+        //sdf_well?????_how???
     }
 }
