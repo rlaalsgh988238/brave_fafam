478d8ab6b72e2fd163c804487174596f6c552fe0
KimMinHo <rlaalsgh988238@naver.com>
2022-07-07 23:28:37 +0900

    new activity

---
+++ app/src/main/java/org/techtown/fafam_brave/mission_page.java
@@ -0,0 +1,14 @@
+package org.techtown.fafam_brave;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class mission_page extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mission_page);
+    }
+}
